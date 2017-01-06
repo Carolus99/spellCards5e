a4fcfdcc888ae5d806a0273d1ee44fc71f5fd0ed
Carolus99 <chasecomfort@gmail.com>
2017-01-06 14:56:26 -0800

    model creation
    created model system for new spells, removed class from views.py

--- SpellCards/main_app/views.py
+++ SpellCards/main_app/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
-
+from .models import Spell
 # Create your views here.
+
+
 def testindex(request):
 
     return render(request, 'testindex.html', {'spells':spells})
 
 def index(request):
-
+    spells = Spell.objects.all()
     return render(request, 'index.html', {'spells':spells})
-
-class Spell:
-    def __init__(self, name, level, distance, school):
-        self.name = name
-        self.level = level
-        self.distance = distance
-        self.school = school
-
-spells = [
-    Spell('Aid', 2, 30, 'Abjuration'),
-    Spell('Antimagic Field', 8, 'Self', 'Abjuration'),
-    Spell('Blade Barrier', 6, 30, 'Evocation'),
-    Spell('Blur', 2, 'Self', 'Illusion')
-]
